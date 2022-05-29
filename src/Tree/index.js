738b4f553f654ff80b9db6cdc69b8e745644caca
Amara Alekhya <anv.alekhya@gmail.com>
2022-05-29 22:01:04 +0530

    first commit

---
+++ src/Tree/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Branch from './Branch';
+
+const Tree = ({data}) =>{
+    return(
+        <div>
+            {data.map((item) => <Branch key={item.id} item={item} level={0} />)}
+        </div>
+    );
+}
+
+export default Tree;
---
+++ src/constant.js
@@ -0,0 +1,67 @@
+export const DATA = [
+    {
+        id: '1',
+        label: 'Item1',
+        children:[
+            {
+                id: '2',
+                label: 'Item1.1',
+                children:[
+                    {
+                        id: '3',
+                        label: 'Item1.1.1',
+                        
+                    },
+                    {
+                        id: '4',
+                        label: 'Item1.1.2',
+                        
+                    },
+                ],
+            
+                id: '5',
+                label: 'Item1.2',
+                        
+            },
+            {
+                id: '6',
+                label: 'Item1.3',
+                        
+            },
+                
+    
+    
+
+        ],
+    },
+    {
+        id: '7',
+        label: 'Item2',
+        children:[
+            {
+                id: '8',
+                label: 'Item2.1',
+            },
+        
+        ],
+    },
+    {
+        id: '9',
+        label: 'Item3',
+        children:[
+            {
+                id: '10',
+                label: 'Item3.1',
+                children:[
+                    {
+                        id: '11',
+                        label: 'Item3.1.1',
+                    },
+                ],
+        
+                
+            },
+        ],
+    },
+        
+];
